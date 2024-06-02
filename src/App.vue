fc1ce4fc57608372f81b4159b60f5afdb8f81a40
sammythedeveloper <justsammy484@gmail.com>
2024-06-02 10:30:00 -0400

    chore: initialize Vue frontend

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { onMounted } from 'vue';
+import { useThemeStore } from './stores/theme';
+
+const theme = useThemeStore();
+onMounted(() => theme.theme);
+</script>
+
+<template>
+  <router-view />
+</template>
